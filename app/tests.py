a527b0ab567d4fc51bf975229abd1e5f8f2d22e7
mansinayakpb <mnayak@bestpeers.com>
2024-12-17 15:30:59 +0530

    Task 2 management (#2)
    * add the changed the urls
    
    * create testcases for views
    
    * updated the changes

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,228 @@
-from django.test import TestCase  # noqa
+from datetime import date
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Task
+
+
+class HomeViewTest(TestCase):
+    def test_home_page(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+
+
+class CreateTaskViewTest(TestCase):
+    def setUp(self):
+        self.User = get_user_model()
+        self.assigned_by_user = self.User.objects.create_user(
+            username="taskcreate",
+            email="testuser@example.com",
+            password="Str0ngPa$$w0rd!",
+        )
+        self.assigned_to_user = self.User.objects.create_user(
+            username="taskassign",
+            email="assign@gmail.com",
+            password="password123",
+        )
+        self.url = reverse("create_task")
+
+    def test_get_assign_task_view(self):
+        data = {
+            "title": "Test Task",
+            "assigned_to": self.assigned_to_user.id,
+            "due_date": "2024-12-31",
+            "detail": "Test Task Description",
+            "status": "Inprogress",
+            "priority": "Major",
+        }
+
+        self.client.login(
+            username="testuser@example.com", password="Str0ngPa$$w0rd!"
+        )
+
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("task_list"))
+
+        task = Task.objects.get(title="Test Task")
+        self.assertEqual(task.assigned_by, self.assigned_by_user)
+        self.assertEqual(task.assigned_to, self.assigned_to_user)
+        self.assertEqual(task.detail, "Test Task Description")
+
+    def test_create_task_post_invalid(self):
+        # Test invalid POST request to create a task (missing title)
+        data = {
+            "title": "",
+            "assigned_to": self.assigned_to_user.id,
+            "due_date": "2024-12-31",
+            "detail": "Test Task Description",
+            "status": "Inprogress",
+            "priority": "Major",
+        }
+        self.client.login(
+            username="testuser@example.com", password="Str0ngPa$$w0rd!"
+        )
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "create_task.html")
+        self.assertTrue(response.context["form"].errors)
+
+
+class TaskUpdateViewTest(TestCase):
+    def setUp(self):
+        self.User = get_user_model()
+        self.assigned_by_user = self.User.objects.create_user(
+            username="taskcreate",
+            email="testuser@example.com",
+            password="Str0ngPa$$w0rd!",
+        )
+        self.assigned_to_user = self.User.objects.create_user(
+            username="taskassign",
+            email="assign@gmail.com",
+            password="password123",
+        )
+        self.task = Task.objects.create(
+            title="Test Task",
+            detail="Test Task Description",
+            assigned_to=self.assigned_to_user,
+            assigned_by=self.assigned_by_user,
+            status="Inprogress",
+            priority="Major",
+            due_date="2024-12-31",
+        )
+        self.url = reverse("edit_task", kwargs={"pk": self.task.pk})
+
+    def test_get_update_task(self):
+        self.client.login(
+            username="testuser@example.com", password="Str0ngPa$$w0rd!"
+        )
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "task_update.html")
+
+    def test_post_task_update_success(self):
+        data = {
+            "title": "Task 2",
+            "detail": "Task 2 description",
+            "status": "Inprogress",
+            "priority": "Major",
+            "assigned_to": self.assigned_to_user.id,
+            "due_date": "2025-01-01",
+        }
+        self.client.login(username="creator@gmail.com", password="password123")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 302)
+
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.title, "Task 2")
+        self.assertEqual(self.task.detail, "Task 2 description")
+        self.assertEqual(self.task.status, "Inprogress")
+        self.assertEqual(self.task.priority, "Major")
+        self.assertRedirects(response, reverse("task_list"))
+
+
+class TaskListViewTest(TestCase):
+    def setUp(self):
+        self.User = get_user_model()
+        self.user1 = self.User.objects.create_user(
+            username="testuser1",
+            email="user@gmail.com",
+            password="password123",
+        )
+        self.user2 = self.User.objects.create_user(
+            username="testuser2",
+            email="user2@gmail.com",
+            password="password123",
+        )
+
+        self.task1 = Task.objects.create(
+            title="Task 1",
+            detail="Task 1 description",
+            assigned_to=self.user1,
+            assigned_by=self.user2,
+            status="Complete",
+            priority="Major",
+            due_date=date(2025, 2, 1),
+        )
+
+        self.task2 = Task.objects.create(
+            title="Task 2",
+            detail="Task 2 description",
+            assigned_to=self.user2,
+            assigned_by=self.user1,
+            status="Inprogress",
+            priority="Major",
+            due_date=date(2025, 2, 1),
+        )
+        self.url = reverse("task_list")
+
+    def test_get_request_all_tasks(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "task_list.html")
+        self.assertEqual(len(response.context["task"]), 2)
+
+    def test_get_request_filter_assigned_to(self):
+        response = self.client.get(self.url, {"assigned_to": self.user1.id})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "task_list.html")
+        self.assertEqual(len(response.context["task"]), 1)
+        self.assertEqual(response.context["task"][0].assigned_to, self.user1)
+
+    def test_get_request_filter_status(self):
+        response = self.client.get(self.url, {"status": "Complete"})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "task_list.html")
+        self.assertEqual(len(response.context["task"]), 1)
+        self.assertEqual(response.context["task"][0].status, "Complete")
+
+
+class TaskStatusUpdateViewTest(TestCase):
+
+    def setUp(self):
+        self.User = get_user_model()
+        self.assigned_by_user = self.User.objects.create_user(
+            username="taskcreate",
+            email="testuser@example.com",
+            password="Str0ngPa$$w0rd!",
+        )
+        self.assigned_to_user = self.User.objects.create_user(
+            username="taskassign",
+            email="assign@gmail.com",
+            password="password123",
+        )
+        self.task = Task.objects.create(
+            title="Task 1",
+            detail="Task 1 description",
+            assigned_to=self.assigned_to_user,
+            assigned_by=self.assigned_by_user,
+            status="Inprogress",
+            priority="Major",
+            due_date=date(2025, 1, 1),
+        )
+        self.url = reverse("status_update", kwargs={"pk": self.task.pk})
+
+    def test_get_update_task(self):
+        self.client.login(
+            username="testuser@example.com", password="Str0ngPa$$w0rd!"
+        )
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "status_update.html")
+
+    def test_post_task_status_update(self):
+        data = {
+            "status": "Complete",
+        }
+        self.client.login(
+            username="testuser@example.com", password="Str0ngPa$$w0rd!"
+        )
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 302)
+
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.status, "Complete")
+        self.assertRedirects(response, reverse("task_list"))
--- app/views.py
+++ app/views.py
@@ -9,21 +9,37 @@ from .models import Comment, Task, User
 
 
 class Home(TemplateView):
+    """
+    View to render the home page.
+    """
+
     template_name = "home.html"
 
     def get(self, request):
+        """
+        Handle GET requests for the home page.
+        """
         return render(request, self.template_name)
 
 
 class SignUpView(TemplateView):
+    """
+    View to handle user registration.
+    """
+
     template_name = "signin/register.html"
 
     def get(self, request):
+        """
+        Render the registration form for a GET request.
+        """
         form = SignUpForm()
         return render(request, self.template_name, {"form": form})
 
     def post(self, request):
-        # Process the form submission
+        """
+        Process the registration form for a POST request.
+        """
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
@@ -31,20 +47,29 @@ class SignUpView(TemplateView):
                 request, "Account created successfully! Please log in."
             )
             return redirect("login")
-
         return render(request, self.template_name, {"form": form})
 
 
 class LoginView(TemplateView):
+    """
+    View to handle user login.
+    """
+
     template_name = "signin/login.html"
 
     def get(self, request):
+        """
+        Render the login form. Redirects if the user is already authenticated.
+        """
         if request.user.is_authenticated:
             return redirect("home")
         form = LoginForm()
         return render(request, self.template_name, {"form": form})
 
     def post(self, request):
+        """
+        Authenticate the user and log them in if credentials are valid.
+        """
         form = LoginForm(data=request.POST)
         if form.is_valid():
             email = form.cleaned_data.get("username")
@@ -52,113 +77,143 @@ class LoginView(TemplateView):
             user = authenticate(request, username=email, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("listsoftask")
+                return redirect("task_list")
         return render(request, self.template_name, {"form": form})
 
 
 class LogoutView(View):
+    """
+    View to log out the user.
+    """
+
     def get(self, request):
+        """
+        Log out the user and redirect to the home page.
+        """
         logout(request)
         return redirect("home")
 
 
 class CreateTaskView(TemplateView):
+    """
+    View to create a new task.
+    """
+
     template_name = "create_task.html"
 
     def get(self, request):
+        """
+        Render the task creation form.
+        """
         users = User.objects.exclude(id=request.user.id)
         form = TaskForm()
-        context = {
-            "form": form,
-            "users": users,
-        }
+        context = {"form": form, "users": users}
         return render(request, self.template_name, context=context)
 
     def post(self, request):
+        """
+        Process the task creation form.
+        """
         form = TaskForm(request.POST)
         if form.is_valid():
             task = form.save(commit=False)
             task.assigned_by = request.user
             task.save()
-
-            # Send mail using the utility function
             send_task_assigned_email(task.assigned_to, task)
-
-            return redirect("listsoftask")
-        else:
-            return render(request, self.template_name, {"form": form})
+            return redirect("task_list")
+        return render(request, self.template_name, {"form": form})
 
 
 class UpdateTaskView(TemplateView):
+    """
+    View to update an existing task.
+    """
+
     template_name = "task_update.html"
 
     def get(self, request, pk):
+        """
+        Render the task update form with existing task data.
+        """
         task = Task.objects.filter(id=pk).first()
         form = TaskForm(instance=task)
-        context = {
-            "form": form,
-        }
-        return render(request, self.template_name, context=context)
+        return render(request, self.template_name, {"form": form})
 
     def post(self, request, pk):
+        """
+        Process the task update form.
+        """
         task = Task.objects.filter(id=pk).first()
-
         form = TaskForm(request.POST, instance=task)
-        context = {
-            "form": form,
-            "task": task,
-        }
         if form.is_valid():
             task_update = form.save()
             if task_update.assigned_to:
                 send_task_update_email(task_update)
-            return redirect("listsoftask")
-        return render(request, self.template_name, context=context)
+            return redirect("task_list")
+        return render(
+            request, self.template_name, {"form": form, "task": task}
+        )
 
 
 class DeleteTaskView(TemplateView):
+    """
+    View to delete a task.
+    """
+
     def post(self, request, pk):
+        """
+        Handle task deletion.
+        """
         task = Task.objects.filter(id=pk).first()
         task.delete()
-        return redirect("listsoftask")
+        return redirect("task_list")
 
 
 class DetailTaskView(TemplateView):
+    """
+    View to display details of a specific task.
+    """
+
     template_name = "task_detail.html"
 
     def get(self, request, pk):
+        """
+        Display the task details and associated comments.
+        """
         task = Task.objects.get(id=pk)
         comments = Comment.objects.filter(task=task)
         form = CommentForm()
-        context = {
-            "task": task,
-            "comments": comments,
-            "form": form,
-        }
+        context = {"task": task, "comments": comments, "form": form}
         return render(request, self.template_name, context=context)
 
     def post(self, request, pk):
+        """
+        Add a new comment to the task.
+        """
         task = Task.objects.get(id=pk)
         form = CommentForm(request.POST)
         if form.is_valid():
-            text = form.save(commit=False)
-            text.user = request.user
-            text.task = task
-            text.save()
-            return redirect("listsoftask")
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.task = task
+            comment.save()
+            return redirect("task_list")
         comments = Comment.objects.filter(task=task)
-        context = {
-            "task": task,
-            "comments": comments,
-            "form": form,
-        }
+        context = {"task": task, "comments": comments, "form": form}
         return render(request, self.template_name, context)
 
 
 class TaskListView(TemplateView):
+    """
+    View to display a list of tasks with optional filtering.
+    """
+
     template_name = "task_list.html"
 
     def get(self, request):
+        """
+        Display the task list with filters for assigned user, status, and due date.
+        """
         users = User.objects.all()
         task = Task.objects.all()
 
@@ -184,18 +239,32 @@ class TaskListView(TemplateView):
 
 
 class UserListView(TemplateView):
+    """
+    View to display a list of users.
+    """
+
     template_name = "user_list.html"
 
     def get(self, request):
+        """
+        Display all users.
+        """
         user = User.objects.all()
         context = {"user": user}
         return render(request, self.template_name, context=context)
 
 
 class TaskStatusUpdateView(TemplateView):
+    """
+    View to update the status of a task.
+    """
+
     template_name = "status_update.html"
 
     def get(self, request, pk):
+        """
+        Render the task status update form.
+        """
         task = Task.objects.filter(id=pk).first()
         form = TaskStatusForm(instance=task)
         return render(
@@ -203,11 +272,14 @@ class TaskStatusUpdateView(TemplateView):
         )
 
     def post(self, request, pk):
+        """
+        Process the task status update form.
+        """
         task = Task.objects.filter(id=pk).first()
         form = TaskStatusForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            return redirect("listsoftask")
+            return redirect("task_list")
         return render(
             request, self.template_name, {"form": form, "task": task}
         )
