5148f9861f21e41e6de5611556cb5546031178c9
mansi <mnayak@bestpeers.com>
2024-12-17 15:21:41 +0530

    updated the changes

--- app/urls.py
+++ app/urls.py
@@ -21,26 +21,26 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("createtask/", CreateTaskView.as_view(), name="createtask"),
-    path("tasklist/", TaskListView.as_view(), name="listsoftask"),
+    path("create/task/", CreateTaskView.as_view(), name="create_task"),
+    path("list/task/", TaskListView.as_view(), name="task_list"),
     path(
         "taskdetail/task/<int:pk>/",
         DetailTaskView.as_view(),
-        name="detailtask",
+        name="detail_task",
     ),
     path(
-        "taskupdate/task/<int:pk>/", UpdateTaskView.as_view(), name="editask"
+        "taskupdate/task/<int:pk>/", UpdateTaskView.as_view(), name="edit_task"
     ),
     path(
-        "deletetask/task/<int:task_id>",
+        "delete/task/<int:task_id>",
         DeleteTaskView.as_view(),
-        name="deletetask",
+        name="delete_task",
     ),
-    path("userlist/", UserListView.as_view(), name="userlist"),
+    path("userlist/", UserListView.as_view(), name="user_list"),
     path(
-        "update/task/status/<int:pk>/",
+        "update/status/<int:pk>/",
         TaskStatusUpdateView.as_view(),
-        name="statusupdate",
+        name="status_update",
     ),
 ]
 
